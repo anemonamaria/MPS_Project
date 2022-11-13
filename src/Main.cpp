fe2838f00f1cfd245fed4a7d95ec55d7bad313d1
bogpie <bogdanpielemail@gmail.com>
2022-11-13 21:50:35 +0200

    Created CLion project

---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Bogdan on 13-Nov-22.
+//
+
+#include <iostream>
+#include "Main.h"
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ src/Main.h
@@ -0,0 +1,15 @@
+//
+// Created by Bogdan on 13-Nov-22.
+//
+
+#ifndef MPS_PROJECT_MAIN_H
+#define MPS_PROJECT_MAIN_H
+
+using namespace std;
+
+class Main {
+
+};
+
+
+#endif //MPS_PROJECT_MAIN_H
