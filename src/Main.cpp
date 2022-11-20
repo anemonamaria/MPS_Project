c849b0e891bbec317f903513bc12f1fa89c9a667
bogpie <bogdanpielemail@gmail.com>
2022-11-20 15:52:05 +0200

    parsed each line of a CSV file

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,46 @@
 //
 // Created by Bogdan on 13-Nov-22.
-//
+// [PalmLeaf-PL1]WN-P1b.csv
 
 #include <iostream>
 #include "Main.h"
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 int main() {
-    cout << "Hello, World!" << endl;
+    fstream file;
+    file.open("input/mps-local/[DIBCO_2019]6.CSV", ios::in);
+
+    if (!file) {
+        cout << "Error opening file";
+        return 1;
+    }
+
+    string line;
+    while (file.peek() != EOF) {
+        getline(file, line);
+        if (line.empty()) {
+            continue;
+        }
+
+        // split line by comma
+        string delimiter = ",";
+
+        size_t pos = 0;
+        string token;
+        while ((pos = line.find(delimiter)) != string::npos) {
+            token = line.substr(0, pos);
+            double value = stod(token);
+            cout << value << " ";
+            line.erase(0, pos + delimiter.length());
+        }
+        // Last value has no comma afterwards
+        double value = stod(line);
+        cout << value << " ";
+        cout << '\n';
+    }
+
+    file.close();
     return 0;
 }
